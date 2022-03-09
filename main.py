61da7a6eb63fb6af0354243cf458cb83b8abe8b8
dlans <wy2857749@163.com>
2022-03-09 16:09:56 +0800

    re code style

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import os
 
 import myUtil
 
-myUtil.filesRename(os.getcwd() + "\\dataset\\train\\image", "image_train_", ".jpg")
-myUtil.filesRename(os.getcwd() + "\\dataset\\test\\image", "image_test_", ".jpg")
 
-path = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\BaoManBuDongGan_Yin_47.jpg"
-savePath = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\tailoring"
-myUtil.imageTailoring(path, savePath, 300, 400)
+def main():
+    myUtil.files_rename(os.getcwd() + "\\dataset\\train\\image", "image_train_", ".jpg")
+    myUtil.files_rename(os.getcwd() + "\\dataset\\test\\image", "image_test_", ".jpg")
+
+    path = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\BaoManBuDongGan_Yin_47.jpg"
+    savePath = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\tailoring"
+    myUtil.image_tailoring(path, savePath, 300, 400)
+
+
+if __name__ == "__main__":
+    main()
--- myUtil.py
+++ myUtil.py
@@ -1,85 +1,91 @@
 import os
 import cv2
 
-def filesRename(path, startName, suffix):
+def files_rename(path: str, start_name: str, suffix: str):
     """文件批量重命名
-    param path: 文件所在目录
-    param startName: 批量重命名的前缀
-    param suffix: 文件后缀名
+    :param path: 文件所在目录
+    :param startName: 批量重命名的前缀
+    :param suffix: 文件后缀名
     """
     count = 0
     num = 0
-    fileList = os.listdir(path)
+    file_list = os.listdir(path)
 
-    for files in fileList:
-        oldDir = os.path.join(path, files)
+    for files in file_list:
+        old_dir = os.path.join(path, files)
 
-        if os.path.isdir(oldDir):
+        if os.path.isdir(old_dir):
             continue
 
         string = str(num)
         if len(string) == 1:
             string = "0" + string
 
-        newDir = os.path.join(path, startName + string + suffix)
-        if newDir == oldDir:
+        new_dir = os.path.join(path, start_name + string + suffix)
+        if new_dir == old_dir:
             num += 1
             continue
 
         try:
-            os.rename(oldDir, newDir)
+            os.rename(old_dir, new_dir)
         except FileExistsError:
-            print(newDir + "已存在，跳过")
+            print(new_dir + "已存在，跳过")
         count += 1
         num += 1
     
     print("完成，共修改了 " + str(count) + " 个文件")
 
 
-def imageTailoring(imgPath, savePath, xStep, yStep):
-    img = cv2.imread(imgPath)
+def image_tailoring(img_path: str, save_path: str, h_step: int, w_step: int):
+    """将图片进行裁剪
+    :param img_path: 要裁剪的图片的完整路径
+    :param save_path: 指定裁剪后文件的存放目录
+    :param h_step: 裁剪后子图片的高度
+    :param w_step: 裁剪后子图片的长度
+    """
+    img = cv2.imread(img_path)
     if img is None:
         print("Error:file not found")
         return
     
     try:
-        os.mkdir(savePath)
+        os.mkdir(save_path)
     except FileExistsError:
         pass
 
-    imgName = imgPath.split("\\")[-1].split(".")[0]
-    xNum = 1
-    yNum = 1
-    xStart = 0
-    xEnd = xStep
-    yStart = 0
-    yEnd = yStep
-    while xNum <= int(img.shape[0] / xStep):
-        while yNum <= int(img.shape[1] / yStep):
-            imgCropped = img[xStart:xEnd, yStart:yEnd]
+    img_name = img_path.split("\\")[-1].split(".")[0]
+    x_num = 1
+    y_num = 1
+    x_start = 0
+    x_end = h_step
+    y_start = 0
+    y_end = w_step
+    while x_num <= int(img.shape[0] / h_step):
+        while y_num <= int(img.shape[1] / w_step):
+            img_cropped = img[x_start:x_end, y_start:y_end]
 
-            stringXNum = str(xNum)
-            if len(stringXNum) == 1:
-                stringXNum = "0" + stringXNum
-            stringYNum = str(yNum)
-            if len(stringYNum) == 1:
-                stringYNum = "0" + stringYNum
-            stringNumber = "_" + stringXNum + "_" + stringYNum
+            str_xnum = str(x_num)
+            if len(str_xnum) == 1:
+                str_xnum = "0" + str_xnum
+            str_ynum = str(y_num)
+            if len(str_ynum) == 1:
+                str_ynum = "0" + str_ynum
+            str_number = "_" + str_xnum + "_" + str_ynum
 
-            imgSaveName = savePath + "\\" + imgName + stringNumber + ".jpg"
-            if os.path.isfile(imgSaveName):
-                os.remove(imgSaveName)
-            cv2.imwrite(imgSaveName, imgCropped)
+            img_savename = save_path + "\\" + img_name + str_number + ".jpg"
+            if os.path.isfile(img_savename):
+                os.remove(img_savename)
+            cv2.imwrite(img_savename, img_cropped)
 
-            yStart += yStep
-            yEnd += yStep
-            yNum += 1
-        xStart += xStep
-        xEnd += xStep
-        yStart = 0
-        yEnd = yStep
-        xNum += 1
-        yNum = 1
+            y_start += w_step
+            y_end += w_step
+            y_num += 1
+        x_start += h_step
+        x_end += h_step
+        y_start = 0
+        y_end = w_step
+        x_num += 1
+        y_num = 1
     
     print("tailoring finished, check")
 
