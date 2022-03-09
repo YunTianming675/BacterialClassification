ca8cb57ef2fe07ffb08b2f45ce796827549c173f
dlans <wy2857749@163.com>
2022-03-09 11:47:48 +0800

    add function:image_toilaring;code restyle

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import myUtil
 
 myUtil.filesRename(os.getcwd() + "\\dataset\\train\\image", "image_train_", ".jpg")
 myUtil.filesRename(os.getcwd() + "\\dataset\\test\\image", "image_test_", ".jpg")
+
+path = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\BaoManBuDongGan_Yin_47.jpg"
+savePath = r"D:\\Project\\VSCode\\Python\\ImageTest01\\dataset\\train_02\\tailoring"
+myUtil.imageTailoring(path, savePath, 300, 400)
--- myUtil.py
+++ myUtil.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 def filesRename(path, startName, suffix):
     """文件批量重命名
@@ -33,4 +34,52 @@ def filesRename(path, startName, suffix):
         num += 1
     
     print("完成，共修改了 " + str(count) + " 个文件")
+
+
+def imageTailoring(imgPath, savePath, xStep, yStep):
+    img = cv2.imread(imgPath)
+    if img is None:
+        print("Error:file not found")
+        return
+    
+    try:
+        os.mkdir(savePath)
+    except FileExistsError:
+        pass
+
+    imgName = imgPath.split("\\")[-1].split(".")[0]
+    xNum = 1
+    yNum = 1
+    xStart = 0
+    xEnd = xStep
+    yStart = 0
+    yEnd = yStep
+    while xNum <= int(img.shape[0] / xStep):
+        while yNum <= int(img.shape[1] / yStep):
+            imgCropped = img[xStart:xEnd, yStart:yEnd]
+
+            stringXNum = str(xNum)
+            if len(stringXNum) == 1:
+                stringXNum = "0" + stringXNum
+            stringYNum = str(yNum)
+            if len(stringYNum) == 1:
+                stringYNum = "0" + stringYNum
+            stringNumber = "_" + stringXNum + "_" + stringYNum
+
+            imgSaveName = savePath + "\\" + imgName + stringNumber + ".jpg"
+            if os.path.isfile(imgSaveName):
+                os.remove(imgSaveName)
+            cv2.imwrite(imgSaveName, imgCropped)
+
+            yStart += yStep
+            yEnd += yStep
+            yNum += 1
+        xStart += xStep
+        xEnd += xStep
+        yStart = 0
+        yEnd = yStep
+        xNum += 1
+        yNum = 1
+    
+    print("tailoring finished, check")
 
